9dbc2561402f8c3cbefdc118f277c53d55fa92d6
phrack <project.shootoff@gmail.com>
2015-04-05 17:59:32 -0400

    Initial commit

---
+++ src/main/java/com/shootoff/Main.java
@@ -0,0 +1,20 @@
+package com.shootoff;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			// TODO: Fill me in
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
