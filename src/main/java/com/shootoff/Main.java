79e9d946c4956a8532b5f3306412c05ce822135f
phrack <project.shootoff@gmail.com>
2015-04-07 20:47:50 -0400

    Created main window. This is progress towards #1. Still need preferences window and arena.

--- src/main/java/com/shootoff/Main.java
+++ src/main/java/com/shootoff/Main.java
@@ -1,17 +1,24 @@
 package com.shootoff;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			// TODO: Fill me in
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/com/shootoff/gui/ShootOFF.fxml"));
+	       
+		Scene scene = new Scene(root, 749, 534);
+	    
+		primaryStage.setTitle("ShootOFF");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) {
