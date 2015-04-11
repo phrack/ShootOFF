bafbc3279d0d407118c0a15ab1c606169be59358
phrack <project.shootoff@gmail.com>
2015-04-11 19:54:17 -0400

    Initial commit of canvas manager. This starts #3

---
+++ src/main/java/com/shootoff/gui/CanvasManager.java
@@ -0,0 +1,37 @@
+package com.shootoff.gui;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.shootoff.camera.Shot;
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+
+public class CanvasManager {
+	private final Canvas canvas;
+	private final GraphicsContext gc;
+	private final List<Shot> shots = new ArrayList<Shot>();
+	
+	public CanvasManager(Canvas canvas) {
+		this.canvas = canvas;
+		this.gc = canvas.getGraphicsContext2D();
+	}
+	
+	public void updateBackground(Image img) {
+		canvas.getGraphicsContext2D().drawImage(img, 0, 0);
+		drawShots();
+	}
+	
+	public void addShot(Shot shot) {
+		shots.add(shot);
+		shot.drawShot(gc);
+	}
+	
+	private void drawShots() {
+		for (Shot shot : shots) {
+			shot.drawShot(gc);
+		}
+	}
+}
