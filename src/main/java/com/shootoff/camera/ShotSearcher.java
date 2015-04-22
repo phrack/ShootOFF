e88fc1b6860d1adfa47156d2a8d517c56129d6c6
phrack <project.shootoff@gmail.com>
2015-04-22 19:52:53 -0400

    Added missing copyright notices.

--- src/main/java/com/shootoff/camera/ShotSearcher.java
+++ src/main/java/com/shootoff/camera/ShotSearcher.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2015 phrack. All rights reserved.
+ * Use of this source code is governed by a BSD-style license that can be
+ * found in the LICENSE file.
+ */
+
 package com.shootoff.camera;
 
 import java.awt.image.BufferedImage;
