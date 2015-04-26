c899163f251bceabc94e60a901a138777f010d5e
phrack <project.shootoff@gmail.com>
2015-04-26 17:28:24 -0400

    Added webcam configuration GUI. This is progress on #31

--- src/main/java/com/shootoff/gui/PreferencesController.java
+++ src/main/java/com/shootoff/gui/PreferencesController.java
@@ -7,22 +7,33 @@
 package com.shootoff.gui;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
+import com.github.sarxos.webcam.Webcam;
 import com.shootoff.config.Configuration;
 import com.shootoff.config.ConfigurationException;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.Slider;
+import javafx.scene.input.KeyCode;
 import javafx.stage.Stage;
+import javafx.util.Callback;
 
 public class PreferencesController {
+	@FXML private ListView<String> webcamListView;
 	@FXML private Slider detectionRateSlider;
 	@FXML private Label detectionRateLabel;
 	@FXML private Slider laserIntensitySlider;
@@ -39,12 +50,42 @@ public class PreferencesController {
 	
 	private Stage preferencesStage;
 	private Configuration config;
+	private List<Webcam> configuredCameras = new ArrayList<Webcam>();
+	private ObservableList<String> configuredNames = FXCollections.observableArrayList();
 	
 	public void setConfig(Configuration config) throws IOException {
 	    preferencesStage = (Stage)detectionRateSlider.getScene().getWindow();
 	    
 	    ignoreLaserColorChoiceBox.setItems(FXCollections.observableArrayList(
 	    		"None", "red", "green"));
+	    
+	    webcamListView.setCellFactory(new Callback<ListView<String>, 
+	            ListCell<String>>() {
+	                @Override 
+	                public ListCell<String> call(ListView<String> list) {
+	                    return new ImageCell(Webcam.getWebcams());
+	                }
+	            }
+	        );
+		
+	    webcamListView.setOnKeyPressed((event) -> {
+	    		if (event.getCode() == KeyCode.DELETE) {
+	    			ObservableList<String> selectedNames =
+	    					webcamListView.getSelectionModel().getSelectedItems();
+	    			
+	    			for (Iterator<Webcam> it = configuredCameras.iterator(); it.hasNext();) {
+	    				Webcam webcam = it.next();
+	    				if (selectedNames.contains(webcam.getName())) {
+	    					it.remove();
+	    				}
+	    			}
+	    			
+	    			configuredNames.removeAll(selectedNames);
+	    		}
+	    	});
+	    
+	    webcamListView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+	    webcamListView.setItems(configuredNames);
 	    
 		this.config = config;
 		
@@ -84,6 +125,21 @@ public class PreferencesController {
 	@FXML 
 	public void malfunctionsCheckBoxClicked(ActionEvent event) {
 		malfunctionsSlider.setDisable(!malfunctionsCheckBox.isSelected());
+	}
+	
+	@FXML
+	public void addCameraClicked(ActionEvent event) {
+		CameraSelectorScene cameraSelector = new CameraSelectorScene(
+				preferencesStage, configuredCameras);
+		
+		cameraSelector.setOnHidden((e) -> {
+				if (!cameraSelector.getSelectedWebcams().isEmpty()) {
+					for (Webcam webcam : cameraSelector.getSelectedWebcams()) {
+						configuredNames.add(webcam.getName());
+						configuredCameras.add(webcam);
+					}
+				}
+			});
 	}
 	
 	@FXML 
