b366fa28d502501b236c031094b2ba7bdbc232b7
phrack <code.realm@gmail.com>
2015-06-06 13:40:36 -0400

    Added shot debugger GUI code for #100.

---
+++ src/main/java/com/shootoff/gui/StreamDebuggerController.java
@@ -0,0 +1,40 @@
+package com.shootoff.gui;
+
+import com.shootoff.camera.CameraManager;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class StreamDebuggerController implements ThresholdListener {
+	@FXML private ImageView thresholdImageView;
+	@FXML private Slider colorDifferenceSlider;
+	@FXML private Slider centerBorderSlider;
+	@FXML private Slider minDimSlider;
+	@FXML private Slider bloomCountSlider;
+	
+	public void init(CameraManager cameraManager) {
+		cameraManager.setThresholdListener(this);
+		
+		bloomCountSlider.valueProperty().addListener(new ChangeListener<Number>() {
+				@Override 
+				public void changed(ObservableValue<? extends Number> observableValue, Number oldValue, Number newValue) {
+		        	if (newValue == null) return;
+		        
+	        		cameraManager.setBloomCount(newValue.intValue());
+		      	}
+		    });
+	}
+	
+	public ImageView getThresholdImageView() {
+		return thresholdImageView;	
+	}
+
+	@Override
+	public void updateThreshold(Image thresholdImg) {
+		thresholdImageView.setImage(thresholdImg);
+	}
+}
---
+++ src/main/java/com/shootoff/gui/ThresholdListener.java
@@ -0,0 +1,7 @@
+package com.shootoff.gui;
+
+import javafx.scene.image.Image;
+
+public interface ThresholdListener {
+	public void updateThreshold(Image thresholdImg);
+}
