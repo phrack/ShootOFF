a31c0f80b49c8a3d3e4eca16255363f690b7a7d5
phrack <code.realm@gmail.com>
2015-07-12 08:50:15 -0400

    Added ps3eye hardware defaults unit test and logging output + shot time stamps to mock manager

--- src/test/java/com/shootoff/gui/MockCanvasManager.java
+++ src/test/java/com/shootoff/gui/MockCanvasManager.java
@@ -3,6 +3,9 @@ package com.shootoff.gui;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.shootoff.camera.CamerasSupervisor;
 import com.shootoff.camera.Shot;
 import com.shootoff.camera.ShotProcessor;
@@ -14,9 +17,11 @@ import javafx.scene.Group;
 import javafx.scene.paint.Color;
 
 public class MockCanvasManager extends CanvasManager {
+	private final Logger logger = LoggerFactory.getLogger(MockCanvasManager.class);
 	private final List<Shot> shots = new ArrayList<Shot>();
 	private final Configuration config;
 	private final boolean useShotProcessors;
+	private long startTime = 0;
 	
 	public MockCanvasManager(Configuration config) {
 		super(new Group(), config, new CamerasSupervisor(config), FXCollections.observableArrayList());
@@ -34,11 +39,16 @@ public class MockCanvasManager extends CanvasManager {
 
 	@Override
 	public void addShot(Color color, double x, double y) {
-		Shot shot = new Shot(color, x, y, 0, config.getMarkerRadius());
+		if (startTime == 0) startTime = System.currentTimeMillis();
+		Shot shot = new Shot(color, x, y, 
+				System.currentTimeMillis() - startTime, config.getMarkerRadius());
 		
 		if (useShotProcessors) {
 			for (ShotProcessor p : config.getShotProcessors()) {
-				if (!p.processShot(shot)) return;
+				if (!p.processShot(shot)) {
+					logger.info("Processing Shot: Shot Rejected By {}", p.getClass().getName());
+					return;
+				}
 			}
 		}
 		
