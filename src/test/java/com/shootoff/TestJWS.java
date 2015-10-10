7fd99123d60992f712d351e0d68f7a676bea425a
phrack <code.realm@gmail.com>
2015-10-10 19:00:49 -0400

    Added JWS test for #252

---
+++ src/test/java/com/shootoff/TestJWS.java
@@ -0,0 +1,25 @@
+package com.shootoff;
+
+import static org.junit.Assert.*;
+
+import java.util.Optional;
+
+import org.junit.Test;
+
+public class TestJWS {
+	@Test
+	public void testResourcesMetadata() {
+		Main main = new Main();
+		
+		String metadataXML = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"
+				+ "<resources version=\"1.1\" fileSize=\"400319\" />";
+		
+		Optional<Main.ResourcesInfo> ri = main.deserializeMetadataXML(metadataXML);
+		
+		assertTrue(ri.isPresent());
+		
+		assertEquals("1.1", ri.get().getVersion());
+		assertEquals(400319, ri.get().getFileSize());
+		assertEquals(metadataXML, ri.get().getXML());
+	}
+}
