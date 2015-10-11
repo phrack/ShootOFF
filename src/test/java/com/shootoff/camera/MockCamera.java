cc159804a7f5e895d0377d4128df057c86a7d17f
phrack <code.realm@gmail.com>
2015-10-11 10:45:21 -0400

    Added MockCamera for testing to revise #252

---
+++ src/test/java/com/shootoff/camera/MockCamera.java
@@ -0,0 +1,17 @@
+package com.shootoff.camera;
+
+public class MockCamera extends Camera {
+	public MockCamera() {
+		super();
+	}
+	
+	@Override
+	public boolean isOpen() {
+		return true;
+	}
+	
+	@Override
+	public boolean isImageNew() {
+		return false;
+	}
+}
