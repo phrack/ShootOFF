0e38bb231c1dd2200aa914df428b0eee6abfd38a
oluies <orjan.lundberg@gmail.com>
2016-01-10 00:16:49 +0100

    NordicQuickShooting added

---
+++ src/main/java/com/shootoff/plugins/NordicQuickShooting.java
@@ -0,0 +1,272 @@
+/*
+ * ShootOFF - Software for Laser Dry Fire Training
+ * Copyright (C) 2015 phrack
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.shootoff.plugins;
+
+import java.io.File;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javafx.scene.Group;
+import javafx.scene.paint.Color;
+
+import com.shootoff.camera.Shot;
+import com.shootoff.gui.DelayedStartListener;
+import com.shootoff.targets.TargetRegion;
+
+public class NordicQuickShooting extends TrainingExerciseBase implements TrainingExercise, DelayedStartListener {
+	private static final Logger logger = LoggerFactory.getLogger(NordicQuickShooting.class);
+
+	private final static String SCORE_COL_NAME = "Score";
+	private final static int SCORE_COL_WIDTH = 60;
+	private final static String ROUND_COL_NAME = "Round";
+	private final static int ROUND_COL_WIDTH = 80;
+	private final static int START_DELAY = 10; // s
+	private static final int CORE_POOL_SIZE = 4;
+	private ScheduledExecutorService executorService = Executors.newScheduledThreadPool(CORE_POOL_SIZE);
+	private ScheduledFuture<Void> endRound;
+	private TrainingExerciseBase thisSuper;
+	private static int[] ROUND_TIMES = { 150, 20, 10 };
+	private int roundTimeIndex = 0;
+	private int round = 1;
+	private int shotCount = 0;
+	private int runningScore = 0;
+	private Map<Integer, Integer> sessionScores = new HashMap<Integer, Integer>();
+	private int delayMin = 4;
+	private int delayMax = 8;
+	private boolean repeatExercise = true;
+	private boolean coloredRows = false;
+	private boolean testing = false;
+
+	public NordicQuickShooting() {}
+
+	public NordicQuickShooting(List<Group> targets) {
+		super(targets);
+		thisSuper = super.getInstance();
+		setInitialValues();
+	}
+
+	private void setInitialValues() {
+		roundTimeIndex = 0;
+		round = 1;
+		shotCount = 0;
+		runningScore = 0;
+
+		for (int time : ROUND_TIMES) {
+			sessionScores.put(time, 0);
+		}
+	}
+
+	@Override
+	public void init() {
+		super.pauseShotDetection(true);
+		super.getDelayedStartInterval(this);
+
+		startExercise();
+	}
+
+	// For testing
+	protected void init(int delayMin, int delayMax) {
+		this.delayMin = delayMin;
+		this.delayMax = delayMax;
+
+		testing = true;
+
+		startExercise();
+	}
+
+	private void startExercise() {
+		super.addShotTimerColumn(SCORE_COL_NAME, SCORE_COL_WIDTH);
+		super.addShotTimerColumn(ROUND_COL_NAME, ROUND_COL_WIDTH);
+
+		if (!testing) {
+			executorService.schedule(new SetupWait(), START_DELAY, TimeUnit.SECONDS);
+		} else {
+			new SetupWait().call();
+		}
+	}
+
+	@Override
+	public void updatedDelayedStartInterval(int min, int max) {
+		delayMin = min;
+		delayMax = max;
+	}
+
+	private class SetupWait implements Callable<Void> {
+		@Override
+		public Void call() {
+			if (repeatExercise) {
+				TrainingExerciseBase.playSound(new File("sounds/voice/shootoff-makeready.wav"));
+				int randomDelay = new Random().nextInt((delayMax - delayMin) + 1) + delayMin;
+				if (!testing) {
+					executorService.schedule(new StartRound(), randomDelay, TimeUnit.SECONDS);
+				} else {
+					new StartRound().call();
+				}
+			}
+
+			return null;
+		}
+	}
+
+	private class StartRound implements Callable<Void> {
+		@Override
+		public Void call() {
+			shotCount = 0;
+
+			if (repeatExercise) {
+				if (coloredRows) {
+					thisSuper.setShotTimerRowColor(Color.LIGHTGRAY);
+				} else {
+					thisSuper.setShotTimerRowColor(null);
+				}
+
+				coloredRows = !coloredRows;
+
+				TrainingExerciseBase.playSound("sounds/beep.wav");
+				thisSuper.pauseShotDetection(false);
+				endRound = executorService.schedule(new EndRound(), ROUND_TIMES[roundTimeIndex], TimeUnit.SECONDS);
+			}
+
+			return null;
+		}
+	}
+
+	private class EndRound implements Callable<Void> {
+		@Override
+		public Void call() {
+			if (repeatExercise) {
+				thisSuper.pauseShotDetection(true);
+				TrainingExerciseBase.playSound(new File("sounds/voice/shootoff-roundover.wav"));
+
+				int randomDelay = new Random().nextInt((delayMax - delayMin) + 1) + delayMin;
+
+				if (round < 4) {
+					// Go to next round
+					round++;
+					if (!testing) {
+						executorService.schedule(new StartRound(), randomDelay, TimeUnit.SECONDS);
+					} else {
+						new StartRound().call();
+					}
+				} else if (roundTimeIndex < ROUND_TIMES.length - 1) {
+					// Go to round 1 for next time
+					round = 1;
+					roundTimeIndex++;
+					if (!testing) {
+						executorService.schedule(new StartRound(), randomDelay, TimeUnit.SECONDS);
+					} else {
+						new StartRound().call();
+					}
+				} else {
+					TextToSpeech.say("Event over... Your score is " + runningScore);
+					thisSuper.pauseShotDetection(false);
+					// At this point we end and the user has to hit reset to
+					// start again
+				}
+			}
+
+			return null;
+		}
+	}
+
+	@Override
+	public ExerciseMetadata getInfo() {
+		return new ExerciseMetadata("ISSF 25M Standard Pistol", "1.0", "phrack",
+				"This exercise implements the ISSF event describe at: "
+						+ "http://www.pistol.org.au/events/disciplines/issf. You "
+						+ "can use any scored target with this exercise, but use "
+						+ "the ISSF target for the most authentic experience.");
+	}
+
+	@Override
+	public void shotListener(Shot shot, Optional<TargetRegion> hitRegion) {
+		shotCount++;
+
+		int hitScore = 0;
+
+		if (hitRegion.isPresent()) {
+			TargetRegion r = hitRegion.get();
+
+			if (r.tagExists("points")) {
+				hitScore = Integer.parseInt(r.getTag("points"));
+				sessionScores.put(ROUND_TIMES[roundTimeIndex],
+						sessionScores.get(ROUND_TIMES[roundTimeIndex]) + hitScore);
+				runningScore += hitScore;
+			}
+
+			StringBuilder message = new StringBuilder();
+
+			for (Integer time : ROUND_TIMES) {
+				message.append(String.format("%ss score: %d%n", time, sessionScores.get(time)));
+			}
+
+			super.showTextOnFeed(message.toString() + "total score: " + runningScore);
+		}
+
+		String currentRound = String.format("R%d (%ds)", round, ROUND_TIMES[roundTimeIndex]);
+		super.setShotTimerColumnText(SCORE_COL_NAME, String.valueOf(hitScore));
+		super.setShotTimerColumnText(ROUND_COL_NAME, currentRound);
+
+		if (shotCount == 5 && !endRound.isDone()) {
+			try {
+				thisSuper.pauseShotDetection(true);
+				endRound.cancel(true);
+				new EndRound().call();
+			} catch (Exception e) {
+				logger.error("Error ending current ISSF round (five shots detected)", e);
+			}
+		}
+
+	}
+
+	@Override
+	public void reset(List<Group> targets) {
+		super.pauseShotDetection(true);
+
+		repeatExercise = false;
+		executorService.shutdownNow();
+
+		setInitialValues();
+
+		thisSuper.setShotTimerRowColor(null);
+		super.showTextOnFeed("");
+
+		repeatExercise = true;
+		executorService = Executors.newScheduledThreadPool(CORE_POOL_SIZE);
+		executorService.schedule(new SetupWait(), START_DELAY, TimeUnit.SECONDS);
+	}
+
+	@Override
+	public void destroy() {
+		repeatExercise = false;
+		executorService.shutdownNow();
+		super.destroy();
+	}
+}
---
+++ src/test/java/com/shootoff/plugins/TestNordicQuickShooting.java
@@ -0,0 +1,217 @@
+package com.shootoff.plugins;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.PrintStream;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import com.shootoff.camera.CamerasSupervisor;
+import com.shootoff.camera.Shot;
+import com.shootoff.config.Configuration;
+import com.shootoff.config.ConfigurationException;
+import com.shootoff.gui.JavaFXThreadingRule;
+import com.shootoff.gui.ShotEntry;
+import com.shootoff.gui.Target;
+import com.shootoff.targets.TargetRegion;
+import com.shootoff.targets.io.TargetIO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Group;
+import javafx.scene.control.TableView;
+import javafx.scene.paint.Color;
+
+public class TestNordicQuickShooting {
+	@Rule public JavaFXThreadingRule javafxRule = new JavaFXThreadingRule();
+	
+	private PrintStream originalOut;
+	private ByteArrayOutputStream stringOut = new ByteArrayOutputStream();
+	private PrintStream stringOutStream;
+	private List<Group> targetGroups;
+	private ISSFStandardPistol issfExercise;
+	private TargetRegion scoredRegion;
+	private int regionScore;
+	
+	@Before
+	public void setUp() throws UnsupportedEncodingException, ConfigurationException {
+		new JFXPanel(); // Initialize the JFX toolkit
+		
+		stringOutStream = new PrintStream(stringOut, false, "UTF-8");
+		System.setProperty("shootoff.home", System.getProperty("user.dir"));
+		
+		TextToSpeech.silence(true);
+		TrainingExerciseBase.silence(true);
+		originalOut = System.out;
+		System.setOut(stringOutStream);
+		
+		Configuration config = new Configuration(new String[0]);
+		config.setDebugMode(true);
+		
+		CamerasSupervisor cs = new CamerasSupervisor(config);
+		
+		List<Target> targets = new ArrayList<Target>();
+		targetGroups = new ArrayList<Group>();
+		Target issfTarget = new Target(TargetIO.loadTarget(new File("targets/ISSF.target")).get(), targets);
+		targets.add(issfTarget);
+		targetGroups.add(issfTarget.getTargetGroup());
+		scoredRegion = (TargetRegion)issfTarget.getTargetGroup().getChildren().get(0);
+		regionScore = Integer.parseInt(scoredRegion.getTag("points"));
+		
+		issfExercise = new ISSFStandardPistol(targetGroups);
+		TableView<ShotEntry> shotTimerTable = new TableView<ShotEntry>();
+		ObservableList<ShotEntry> shotEntries = FXCollections.observableArrayList();
+		shotEntries.add(new ShotEntry(new Shot(Color.RED, 0, 0, 0, 2), Optional.empty(), Optional.empty(), false, false));
+		shotTimerTable.setItems(shotEntries);
+		issfExercise.init(config, cs, null, shotTimerTable);
+		issfExercise.init(0, 0);
+	}
+	
+	@After
+	public void tearDown() {
+		TextToSpeech.silence(false);
+		TrainingExerciseBase.silence(false);
+		System.setOut(originalOut);
+	}
+	
+	private String getScoreString(int roundOne, int roundTwo, int roundThree, boolean roundOver, boolean gameOver) {
+		String scoreString = String.format("150s score: %d%n20s score: %d%n10s score: %d%ntotal score: %d%n",
+				roundOne, roundTwo, roundThree, roundOne + roundTwo + roundThree);
+		
+		if (gameOver) {
+			return scoreString + String.format("sounds/voice/shootoff-roundover.wav%nEvent over... Your score is 60%n").replace('/', File.separatorChar);
+		}
+		
+		if (roundOver) {
+			return scoreString + String.format("sounds/voice/shootoff-roundover.wav%nsounds/beep.wav%n").replace('/', File.separatorChar);
+		}
+		
+		return scoreString;
+	}
+	
+	@Test
+	public void testFullRound() throws UnsupportedEncodingException {
+		assertEquals(String.format("sounds/voice/shootoff-makeready.wav%nsounds/beep.wav%n"), stringOut.toString("UTF-8").replace(File.separatorChar, '/'));
+		stringOut.reset();
+
+		// 150s round 1-4
+		for (int i = 0; i < 4; i++) {
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			// (regionScore * 5 * i) = last round's score
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 2, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 3, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 4, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 5, 0, 0, true, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+		}
+		
+		// 20s round 1-4
+		for (int i = 0; i < 4; i++) {
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			// (regionScore * 5 * i) = last round's score
+			assertEquals(getScoreString(regionScore * 20, (regionScore * 5 * i) + regionScore, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, (regionScore * 5 * i) + regionScore * 2, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, (regionScore * 5 * i) + regionScore * 3, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, (regionScore * 5 * i) + regionScore * 4, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, (regionScore * 5 * i) + regionScore * 5, 0, true, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+		}
+		
+		// 10s round 1-4
+		for (int i = 0; i < 4; i++) {
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			// (regionScore * 5 * i) = last round's score
+			assertEquals(getScoreString(regionScore * 20, regionScore * 20, (regionScore * 5 * i) + regionScore, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, regionScore * 20, (regionScore * 5 * i) + regionScore * 2, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, regionScore * 20, (regionScore * 5 * i) + regionScore * 3, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, regionScore * 20, (regionScore * 5 * i) + regionScore * 4, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			boolean gameOver = i == 3;
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString(regionScore * 20, regionScore * 20, (regionScore * 5 * i) + regionScore * 5, true, gameOver), stringOut.toString("UTF-8"));
+			stringOut.reset();
+		}
+	}
+	
+	@Test
+	public void testFull150sThenReset() throws UnsupportedEncodingException {
+		assertEquals(String.format("sounds/voice/shootoff-makeready.wav%nsounds/beep.wav%n"), stringOut.toString("UTF-8").replace(File.separatorChar, '/'));
+		stringOut.reset();
+
+		// 150s round 1-4
+		for (int i = 0; i < 4; i++) {
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			// (regionScore * 5 * i) = last round's score
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 2, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 3, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 4, 0, 0, false, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+			
+			issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+			assertEquals(getScoreString((regionScore * 5 * i) + regionScore * 5, 0, 0, true, false), stringOut.toString("UTF-8"));
+			stringOut.reset();
+		}
+		
+		issfExercise.reset(targetGroups);
+		
+		issfExercise.shotListener(new Shot(Color.RED, 0, 0, 0, 2), Optional.of(scoredRegion));
+		// (regionScore * 5 * i) = last round's score
+		assertEquals(String.format("%n") + getScoreString(regionScore, 0, 0, false, false), stringOut.toString("UTF-8"));
+		stringOut.reset();
+	}
+}
