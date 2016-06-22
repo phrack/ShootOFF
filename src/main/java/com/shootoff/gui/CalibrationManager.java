phrack/ShootOFF

commit 813e7fbc3aeb82977ad79c2cd945fe25a7ec9879

Removed arena masking code (at this time we don't plan to finish the feature)

src/main/java/com/shootoff/gui/CalibrationManager.java
@@ -28,8 +28,6 @@ import org.slf4j.LoggerFactory;
 
 import com.shootoff.camera.CameraCalibrationListener;
 import com.shootoff.camera.CameraManager;
-import com.shootoff.camera.arenamask.ArenaMaskManager;
-import com.shootoff.config.Configuration;
 import com.shootoff.gui.controller.ProjectorArenaController;
 import com.shootoff.targets.RectangleRegion;
 import com.shootoff.targets.io.TargetIO;
@@ -250,8 +248,6 @@ public class CalibrationManager implements CameraCalibrationListener {
 		setArenaBackground("pattern.png");
 		isShowingPattern.set(true);
 
-		calibratingCameraManager.enableAutoCalibration(Configuration.USE_ARENA_MASK);
-
 		showAutoCalibrationMessage();
 		
 		launchAutoCalibrationTimer();
@@ -309,10 +305,6 @@ public class CalibrationManager implements CameraCalibrationListener {
 			calibratingCanvasManager.removeDiagnosticMessage(autoCalibrationMessage);
 			autoCalibrationMessage = null;
 		}
-	}
-
-	public void setArenaMaskManager(ArenaMaskManager arenaMaskManager) {
-		arenaController.setArenaMaskManager(arenaMaskManager);
 	}
 
 	private boolean isFullScreen = false;

src/main/java/com/shootoff/camera/CameraCalibrationListener.java
@@ -1,11 +1,8 @@
 package com.shootoff.camera;
 
-import com.shootoff.camera.arenamask.ArenaMaskManager;
-
 import javafx.geometry.Bounds;
 
 public interface CameraCalibrationListener {
 	public void calibrate(Bounds bounds, boolean calibratedFromCanvas);
 	public void setArenaBackground(String resourceFilename);
-	public void setArenaMaskManager(ArenaMaskManager arenaMaskManager);
 }

src/main/java/com/shootoff/camera/CameraManager.java
@@ -37,7 +37,6 @@ import org.opencv.imgproc.Imgproc;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import com.shootoff.camera.arenamask.ArenaMaskManager;
 import com.shootoff.camera.autocalibration.AutoCalibrationManager;
 import com.shootoff.camera.perspective.PerspectiveManager;
 import com.shootoff.camera.shotdetection.ShotDetectionManager;
@@ -131,7 +130,6 @@ public class CameraManager {
 	protected boolean cameraAutoCalibrated = false;
 
 	protected final DeduplicationProcessor deduplicationProcessor = new DeduplicationProcessor(this);
-	protected final ArenaMaskManager arenaMaskManager;
 
 	private CameraCalibrationListener cameraCalibrationListener;
 	
@@ -150,12 +148,6 @@ public class CameraManager {
 	}
 
 	public CameraManager(Camera webcam, CameraErrorView cameraErrorView, CameraView view, Configuration config) {
-		if (Configuration.USE_ARENA_MASK) {
-			arenaMaskManager = new ArenaMaskManager();
-		} else {
-			arenaMaskManager = null;
-		}
-
 		if (webcam != null)
 			this.webcam = Optional.of(webcam);
 		else
@@ -172,12 +164,6 @@ public class CameraManager {
 	}
 
 	protected CameraManager(CameraView view, Configuration config) {
-		if (Configuration.USE_ARENA_MASK) {
-			arenaMaskManager = new ArenaMaskManager();
-		} else {
-			arenaMaskManager = null;
-		}
-
 		this.webcam = Optional.empty();
 		this.cameraErrorView = Optional.empty();
 		this.cameraView = view;
@@ -242,8 +228,6 @@ public class CameraManager {
 	}
 
 	public void close() {
-		if (arenaMaskManager != null) arenaMaskManager.isStreaming.set(false);
-
 		getCameraView().close();
 		setDetecting(false);
 		setStreaming(false);
@@ -633,8 +617,6 @@ public class CameraManager {
 
 			Imgproc.cvtColor(matFrameBGR, matFrameHSV, Imgproc.COLOR_BGR2HSV);
 
-			if (Configuration.USE_ARENA_MASK) arenaMaskManager.updateAvgLums(submatFrameBGR);
-
 			if (debuggerListener.isPresent()) {
 				debuggerListener.get().updateDebugView(Camera.matToBufferedImage(submatFrameBGR));
 			}
@@ -772,13 +754,6 @@ public class CameraManager {
 			cameraAutoCalibrated = true;
 			cameraCalibrationListener.calibrate(bounds, false);
 
-			if (Configuration.USE_ARENA_MASK) {
-				cameraCalibrationListener.setArenaMaskManager(arenaMaskManager);
-
-				shotDetectionManager.setArenaMaskManager(arenaMaskManager);
-				arenaMaskManager.start((int) bounds.getWidth(), (int) bounds.getHeight());
-			}
-
 			if (recordCalibratedArea && !recordingCalibratedArea)
 				startRecordingCalibratedArea(new File("calibratedArea.mp4"), (int) bounds.getWidth(),
 						(int) bounds.getHeight());
@@ -791,8 +766,6 @@ public class CameraManager {
 			acm = new AutoCalibrationManager(this, calculateFrameDelay);
 		isAutoCalibrating.set(true);
 		cameraAutoCalibrated = false;
-		// Turns off using mask
-		shotDetectionManager.setArenaMaskManager(null);
 
 		fireAutoCalibration();
 	}

src/main/java/com/shootoff/camera/arenamask/ArenaMaskManager.java
@@ -1,234 +1,0 @@
-/*
- * ShootOFF - Software for Laser Dry Fire Training
- * Copyright (C) 2016 phrack
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-package com.shootoff.camera.arenamask;
-
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.util.concurrent.Semaphore;
-import java.util.concurrent.atomic.AtomicBoolean;
-
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import com.xuggle.mediatool.IMediaWriter;
-import com.xuggle.mediatool.ToolFactory;
-import com.xuggle.xuggler.ICodec;
-import com.xuggle.xuggler.IPixelFormat;
-import com.xuggle.xuggler.IVideoPicture;
-import com.xuggle.xuggler.video.ConverterFactory;
-import com.xuggle.xuggler.video.IConverter;
-
-public class ArenaMaskManager implements Runnable {
-	private static final Logger logger = LoggerFactory.getLogger(ArenaMaskManager.class);
-
-	private long delay = 0;
-
-	private Mat mask = new Mat();
-	private Size dsize = null;
-
-	private int avgLums = 0;
-	private int minLums;
-	private int maxLums;
-
-	private int[][] lumsMovingAverage;
-
-	public volatile Mask maskFromArena = null;
-
-	public Semaphore sem = new Semaphore(1);
-
-	public AtomicBoolean isStreaming = new AtomicBoolean(true);
-
-	private boolean recordMask = false;
-
-	@Override
-	public void run() {
-		if (logger.isDebugEnabled()) logger.debug("Starting arenaMaskManager thread");
-
-		if (recordMask) startRecordingStream(new File("testingArenaMask.mp4"));
-
-		while (isStreaming.get()) {
-			try {
-				sem.acquire();
-			} catch (InterruptedException e) {
-				continue;
-			}
-			if (maskFromArena == null) {
-				sem.release();
-				continue;
-			}
-
-			Mask nextMask = maskFromArena;
-			long curDelay = System.currentTimeMillis() - nextMask.getTimestamp();
-
-			if (curDelay > delay) {
-				sem.release();
-				continue;
-			}
-
-			handleMask(nextMask);
-		}
-
-		if (recordMask) stopRecordingStream();
-	}
-
-	public void handleMask(Mask nextMask) {
-		Mat nextMat = nextMask.getLumMask(dsize);
-
-		if (recordMask) recordMask(nextMask);
-
-		int nextMaskAvgLum = nextMask.getAvgMaskLum();
-
-		maskFromArena = null;
-		sem.release();
-
-		double scaledMaskAvgLum = (((double) (nextMaskAvgLum - 0) / (double) (255 * 255 - 0))
-				* (double) (maxLums - minLums) + minLums);
-
-		for (int y = 0; y < nextMat.rows(); y++) {
-			for (int x = 0; x < nextMat.cols(); x++) {
-				int[] maskpx = { 0 };
-				mask.get(y, x, maskpx);
-
-				int[] nextmatpx = { 0 };
-				nextMat.get(y, x, nextmatpx);
-
-				double scaler = (double) lumsMovingAverage[x][y] / scaledMaskAvgLum;
-
-				// int newLum = (int) ((double) nextmatpx[0] * ((double)
-				// lumsMovingAverage[x][y] / (double) nextMaskAvgLum));
-				int scaledValue = (int) (((double) (nextmatpx[0] - 0) / (double) (255 * 255 - 0))
-						* (double) (maxLums - minLums) + minLums);
-				scaledValue *= scaler;
-
-				// int normalized = (int)
-				// ((double)(Math.min(Math.max(nextmatpx[0], minLums), maxLums)
-				// - minLums) / (double)(maxLums - minLums));
-				// int newLum = (int) ((double) normalized * ((double) avgLums /
-				// (double) nextMaskAvgLum));
-
-				// maskpx[0] = (((maskpx[0]) + scaledValue) / 2);
-				maskpx[0] = scaledValue;
-
-				/*
-				 * if (scaledValue > lumsMovingAverage[x][y]) maskpx[0] =
-				 * scaledValue; else maskpx[0] = (int) (.95 * maskpx[0]);
-				 */
-				/*
-				 * if (x == 200 && y == 200 && nextmatpx[0] > 0) logger.warn(
-				 * "pixel {} {} - min {} max {} - maskg {} scaledMaskAvgLum {} scaler {} scaledValue {} lumsMovingAverage[x][y] {} nmpx {} avgLums {} nmAvgLum {} mpx {}"
-				 * , x, y, minLums, maxLums, mask.get(y, x)[0],
-				 * scaledMaskAvgLum, scaler, scaledValue,
-				 * lumsMovingAverage[x][y], nextmatpx[0], avgLums,
-				 * nextMaskAvgLum, maskpx[0]);
-				 */
-
-				mask.put(y, x, maskpx);
-			}
-		}
-	}
-
-	public void setDelay(long delay) {
-		this.delay = delay;
-	}
-
-	public void start(int width, int height) {
-		mask = new Mat(height, width, CvType.CV_32S);
-		dsize = new Size(width, height);
-
-		(new Thread(this)).start();
-	}
-
-	public Mat getMask() {
-		return mask;
-	}
-
-	private boolean recordingStream = true;
-	private boolean isFirstStreamFrame = true;
-	private IMediaWriter videoWriterStream;
-	private long recordingStartTime;
-
-	private void recordMask(Mask mask) {
-		if (recordingStream) {
-			BufferedImage image = ConverterFactory.convertToType(mask.getMaskImage(), BufferedImage.TYPE_3BYTE_BGR);
-			IConverter converter = ConverterFactory.createConverter(image, IPixelFormat.Type.YUV420P);
-
-			IVideoPicture frame = converter.toPicture(image, (System.currentTimeMillis() - recordingStartTime) * 1000);
-			frame.setKeyFrame(isFirstStreamFrame);
-			frame.setQuality(0);
-			isFirstStreamFrame = false;
-
-			videoWriterStream.encodeVideo(0, frame);
-		}
-	}
-
-	public void startRecordingStream(File videoFile) {
-		if (logger.isDebugEnabled()) logger.debug("Writing Video Feed To: {}", videoFile.getAbsoluteFile());
-
-		int width = (int) dsize.width;
-		int height = (int) dsize.height;
-		videoWriterStream = ToolFactory.makeWriter(videoFile.getName());
-		videoWriterStream.addVideoStream(0, 0, ICodec.ID.CODEC_ID_H264, width, height);
-		recordingStartTime = System.currentTimeMillis();
-		isFirstStreamFrame = true;
-
-		recordingStream = true;
-	}
-
-	public void stopRecordingStream() {
-		recordingStream = false;
-		videoWriterStream.close();
-	}
-
-	public void setLumsMovingAverage(int[][] lumsMovingAverage) {
-		this.lumsMovingAverage = lumsMovingAverage;
-	}
-
-	public void updateAvgLums(Mat frame) {
-		long lumsCurrentAcrossFrame = 0;
-		long lumsMinimumAcrossFrame = 255 * 255;
-		long lumsMaximumAcrossFrame = 0;
-
-		for (int y = 0; y < frame.rows(); y++) {
-			for (int x = 0; x < frame.cols(); x++) {
-				byte[] px = { 0, 0, 0 };
-				frame.get(y, x, px);
-				int matS = px[1] & 0xFF;
-				int matV = px[2] & 0xFF;
-
-				int curLum = ((255 - matS) * matV);
-
-				lumsCurrentAcrossFrame += curLum;
-
-				if (curLum > lumsMaximumAcrossFrame) {
-					lumsMaximumAcrossFrame = curLum;
-				} else if (curLum < lumsMinimumAcrossFrame) {
-					lumsMinimumAcrossFrame = curLum;
-				}
-			}
-		}
-
-		lumsCurrentAcrossFrame /= frame.rows() * frame.cols();
-		minLums = (int) lumsMinimumAcrossFrame;
-		maxLums = (int) lumsMaximumAcrossFrame;
-		avgLums = ((avgLums * 4) + (int) lumsCurrentAcrossFrame) / 5;
-	}
-}

src/main/java/com/shootoff/camera/arenamask/Mask.java
@@ -1,90 +1,0 @@
-/*
- * ShootOFF - Software for Laser Dry Fire Training
- * Copyright (C) 2016 phrack
- * 
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-package com.shootoff.camera.arenamask;
-
-import java.awt.image.BufferedImage;
-
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.Size;
-import org.opencv.imgproc.Imgproc;
-
-import com.shootoff.camera.Camera;
-
-public class Mask {
-	private BufferedImage bImage;
-	private Mat mask;
-	private final long timestamp;
-
-	public Mask(BufferedImage bImage, long timestamp) {
-		this.bImage = bImage;
-		this.timestamp = timestamp;
-		this.mask = new Mat();
-	}
-
-	private int avgMaskLum = 0;
-
-	public int getAvgMaskLum() {
-		return avgMaskLum;
-	}
-
-	public BufferedImage getMaskImage() {
-		return bImage;
-	}
-
-	public long getTimestamp() {
-		return timestamp;
-	}
-
-	public Mat getLumMask(Size targetSize) {
-		// Indicates it is already initialized
-		if (mask != null && mask.rows() > 0) return mask;
-
-		mask = new Mat((int) targetSize.height, (int) targetSize.width, CvType.CV_32S);
-
-		Mat src = Camera.bufferedImageToMat(bImage);
-		Imgproc.resize(src, src, targetSize);
-
-		bImage = Camera.matToBufferedImage(src);
-
-		Imgproc.cvtColor(src, src, Imgproc.COLOR_BGR2HSV);
-
-		long tmpAvgMaskLum = 0;
-		for (int y = 0; y < src.rows(); y++) {
-			for (int x = 0; x < src.cols(); x++) {
-				byte[] px = { 0, 0, 0 };
-				src.get(y, x, px);
-				int pxS = px[1] & 0xFF;
-				int pxV = px[2] & 0xFF;
-
-				int pxLum = ((255 - pxS) * pxV);
-
-				tmpAvgMaskLum += pxLum;
-
-				int[] dstLum = { pxLum };
-
-				mask.put(y, x, dstLum);
-			}
-		}
-
-		avgMaskLum = (int) (tmpAvgMaskLum / (mask.rows() * mask.cols()));
-
-		return mask;
-	}
-}

src/main/java/com/shootoff/camera/autocalibration/AutoCalibrationManager.java
@@ -37,7 +37,6 @@ import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfPoint2f;
 import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.RotatedRect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
@@ -318,13 +317,6 @@ public class AutoCalibrationManager {
 	 *  try to identify the outline of the projection area 
 	 *  We are only concerned with size, not alignment or angle */
 	public Optional<Pair<Integer,Integer>> findPaperPattern(Mat mat) {
-
-
-		// For debugging
-		Mat traceMat = null;
-		if (logger.isTraceEnabled()) {
-			traceMat = mat.clone();
-		}
 
 		initializeSize(mat.cols(), mat.rows());
 

src/main/java/com/shootoff/camera/shotdetection/ShotDetectionManager.java
@@ -27,7 +27,6 @@ import java.util.Set;
 import javafx.geometry.Bounds;
 import javafx.scene.paint.Color;
 
-import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
 import org.opencv.imgproc.Imgproc;
@@ -39,7 +38,6 @@ import org.slf4j.LoggerFactory;
 import com.shootoff.camera.CameraManager;
 import com.shootoff.camera.CameraView;
 import com.shootoff.camera.Shot;
-import com.shootoff.camera.arenamask.ArenaMaskManager;
 import com.shootoff.config.Configuration;
 
 public final class ShotDetectionManager {
@@ -99,9 +97,6 @@ public final class ShotDetectionManager {
 	// red without having complicated math every pixel
 	private boolean shouldShowBrightnessWarningBool = false;
 
-	private ArenaMaskManager arenaMaskManager = null;
-	private boolean usingArenaMask = false;
-	
 	final PixelClusterManager pixelClusterManager;	
 
 
@@ -142,15 +137,13 @@ public final class ShotDetectionManager {
 		MAXIMUM_THRESHOLD_PIXELS_FOR_AVG = (int) (frameSize * .000976);
 
 		MINIMUM_SHOT_DIMENSION = (int) (frameSize * .000025);
-
-		if (usingArenaMask) arenaMaskManager.setLumsMovingAverage(lumsMovingAverage);
 	}
 
 	public CameraManager getCameraManager() {
 		return cameraManager;
 	}
 
-	private Pixel updateFilter(int currentH, int currentS, int currentV, int mask, int x, int y, boolean detectShots) {
+	private Pixel updateFilter(int currentH, int currentS, int currentV, int x, int y, boolean detectShots) {
 		Pixel result = null;
 
 		final int currentLum = (255 - currentS) * currentV;
@@ -165,7 +158,7 @@ public final class ShotDetectionManager {
 
 		if (detectShots && pixelAboveExcessiveBrightnessThreshold(lumsMovingAverage[x][y])) {
 			brightPixels.add(new Pixel(x, y));
-		} else if (detectShots && pixelAboveThreshold(currentLum, Math.max(mask, lumsMovingAverage[x][y]))) {
+		} else if (detectShots && pixelAboveThreshold(currentLum, lumsMovingAverage[x][y])) {
 			result = new Pixel(x, y, currentH, currentLum, lumsMovingAverage[x][y], colorDistanceFromRed[x][y]);
 		}
 
@@ -346,15 +339,6 @@ public final class ShotDetectionManager {
 		final byte[] workingFramePrimitive = new byte[size];
 		workingFrame.get(0, 0, workingFramePrimitive);
 
-		final int[] maskPrimitive;
-		if (usingArenaMask) {
-			maskPrimitive = new int[workingFrame.cols() * workingFrame.rows()];
-			final Mat mask = arenaMaskManager.getMask();
-			mask.get(0, 0, maskPrimitive);
-		} else {
-			maskPrimitive = null;
-		}
-
 		// In this loop we accomplish both MovingAverage updates AND threshold
 		// pixel detection
 		Parallel.forIndex(0, (SECTOR_ROWS * SECTOR_COLUMNS), 1, new Operation<Integer>() {
@@ -374,13 +358,7 @@ public final class ShotDetectionManager {
 						final int currentS = workingFramePrimitive[(yOffset + x) * channels + 1] & 0xFF;
 						final int currentV = workingFramePrimitive[(yOffset + x) * channels + 2] & 0xFF;
 
-						int maskInt = 0;
-
-						if (usingArenaMask) {
-							maskInt = maskPrimitive[yOffset + x];
-						}
-
-						final Pixel pixel = updateFilter(currentH, currentS, currentV, maskInt, x, y, detectShots);
+						final Pixel pixel = updateFilter(currentH, currentS, currentV, x, y, detectShots);
 
 						if (pixel != null) thresholdPixels.add(pixel);
 					}
@@ -464,21 +442,6 @@ public final class ShotDetectionManager {
 			File outputfile = new File(String.format("shot-%d-%d-%d.png", cameraManager.getFrameCount(), (int) pc.centerPixelX, (int) pc.centerPixelY));
 			filename = outputfile.toString();
 			Highgui.imwrite(filename, debugFrame);
-
-			if (usingArenaMask) {
-				final Mat mask = arenaMaskManager.getMask();
-				final Mat maskGrayscale = new Mat(mask.rows(), mask.cols(), CvType.CV_8UC1);
-
-				for (int a = 0; a < mask.cols(); a++) {
-					for (int b = 0; b < mask.rows(); b++) {
-						maskGrayscale.put(b, a, mask.get(b, a)[0] / 255);
-					}
-				}
-
-				outputfile = new File(String.format("mask-%d-%d.png", (int) pc.centerPixelX, (int) pc.centerPixelY));
-				filename = outputfile.toString();
-				Highgui.imwrite(filename, maskGrayscale);
-			}
 		}
 
 		if ((cameraManager.isLimitingDetectionToProjection() || cameraManager.isCroppingFeedToProjection())
@@ -491,19 +454,5 @@ public final class ShotDetectionManager {
 			cameraView.addShot(color.get(), x, y);
 		}
 
-	}
-
-	public void setArenaMaskManager(final ArenaMaskManager arenaMaskManager) {
-		if (arenaMaskManager == null) {
-			setUsingArenaMask(false);
-		} else {
-			setUsingArenaMask(true);
-			this.arenaMaskManager = arenaMaskManager;
-			arenaMaskManager.setLumsMovingAverage(lumsMovingAverage);
-		}
-	}
-
-	private void setUsingArenaMask(final boolean val) {
-		usingArenaMask = val;
 	}
 }

src/main/java/com/shootoff/config/Configuration.java
@@ -150,12 +150,6 @@ public class Configuration {
 	private VirtualMagazineProcessor magazineProcessor = null;
 	private MalfunctionsProcessor malfunctionsProcessor = null;
 
-	// TODO: This is used at the moment as a constant to determine if the
-	// (current incomplete) masking solution should be used. This setting
-	// will be unnecessary when the masking code is either complete or
-	// removed.
-	public static final boolean USE_ARENA_MASK = false;
-
 	protected Configuration(InputStream configInputStream, String name) throws IOException, ConfigurationException {
 		configInput = configInputStream;
 		configName = name;

commit 0b4521b10b509670931f2db0cd7922e871a4680c

phrack broke auto calibration with 813e7fbc3aeb82977ad79c2cd945fe25a7ec9879

src/main/java/com/shootoff/gui/CalibrationManager.java
@@ -280,6 +280,8 @@ public class CalibrationManager implements CameraCalibrationListener {
 		if (!isShowingPattern.get()) arenaController.saveCurrentBackground();
 		setArenaBackground("pattern.png");
 		isShowingPattern.set(true);
+		
+		calibratingCameraManager.enableAutoCalibration(false);
 
 		showAutoCalibrationMessage();
 