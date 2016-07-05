a5323efd99fb2cd1ead4359afa7c1eea451dd25d
phrack <code.realm@gmail.com>
2016-07-05 18:24:19 -0400

    Fix typo

--- src/main/java/com/shootoff/gui/TargetView.java
+++ src/main/java/com/shootoff/gui/TargetView.java
@@ -108,7 +108,7 @@ public class TargetView implements com.shootoff.targets.Target {
 		this.cameraName = parent.getCameraName();
 
 		targetGroup.setOnMouseClicked((event) -> {
-			// Skip target selection of click to shoot is being used
+			// Skip target selection if click to shoot is being used
 			if (config.inDebugMode() && (event.isShiftDown() || event.isControlDown())) return;
 
 			parent.toggleTargetSelection(Optional.of(this));
