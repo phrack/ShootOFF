54df035661bf8440ce159f0c66839e1b95bbf9ec
phrack <code.realm@gmail.com>
2016-08-15 19:32:25 -0400

    Add steel target bay arena background to close #503

--- src/main/java/com/shootoff/gui/controller/ShootOFFController.java
+++ src/main/java/com/shootoff/gui/controller/ShootOFFController.java
@@ -918,8 +918,9 @@ public class ShootOFFController implements CameraConfigListener, CameraErrorView
 		addDefaultBackground("Hickok45 Autumn", "/arena/backgrounds/hickok45_autumn.gif");
 		addDefaultBackground("Hickok45 Summer", "/arena/backgrounds/hickok45_summer.gif");
 		addDefaultBackground("Indoor Range", "/arena/backgrounds/indoor_range.gif");
-		addDefaultBackground("Outdoor Range", "/arena/backgrounds/outdoor_range.gif");
 		addDefaultBackground("Kiang West Savanna", "/arena/backgrounds/kiang_west_savanna.gif");
+		addDefaultBackground("Outdoor Range", "/arena/backgrounds/outdoor_range.gif");
+		addDefaultBackground("Steel Range Bay", "/arena/backgrounds/steel_range_bay.gif");
 	}
 
 	private void addDefaultBackground(String menuName, String resourceName) {
