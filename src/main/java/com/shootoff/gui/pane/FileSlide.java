8eab28ad2a7703bc3e0d19e9d223665c6ae5e956
phrack <code.realm@gmail.com>
2016-09-05 19:25:53 -0400

    Fix repeated preferences controller instantiation to close #533 and close #525

--- src/main/java/com/shootoff/gui/pane/FileSlide.java
+++ src/main/java/com/shootoff/gui/pane/FileSlide.java
@@ -25,24 +25,29 @@ import javafx.stage.Stage;
 
 public class FileSlide extends Slide {
 	private static final Logger logger = LoggerFactory.getLogger(FileSlide.class);
+	
+	private static PreferencesController preferencesController = null;
 			
 	public FileSlide(Pane parentControls, Pane parentBody, CalibrationConfigurator calibrationConfigurator, 
 			CameraConfigListener configListener, CameraViews cameraViews, Closeable mainWindow) {
 		super(parentControls, parentBody);
 		
 		addSlideControlButton("Preferences", (event) -> {
-			FXMLLoader loader = new FXMLLoader(
-					getClass().getClassLoader().getResource("com/shootoff/gui/Preferences.fxml"));
-			try {
-				loader.load();
-			} catch (IOException e) {
-				logger.error("Cannot load Preferences.fxml", e);
-				return;
-			}
+			if (preferencesController == null) {
+				FXMLLoader loader = new FXMLLoader(
+						getClass().getClassLoader().getResource("com/shootoff/gui/Preferences.fxml"));
+				try {
+					loader.load();
+				} catch (IOException e) {
+					logger.error("Cannot load Preferences.fxml", e);
+					return;
+				}
 
-			final PreferencesController preferencesController = (PreferencesController) loader.getController();
-			preferencesController.setConfig((Stage) parentControls.getScene().getWindow(), 
-					configListener.getConfiguration(), calibrationConfigurator, configListener);
+				preferencesController = (PreferencesController) loader.getController();
+				preferencesController.setConfig((Stage) parentControls.getScene().getWindow(),
+						configListener.getConfiguration(), calibrationConfigurator, configListener);
+			}
+			
 			final PreferencesSlide preferencesSlide = new PreferencesSlide(parentControls, parentBody, preferencesController);
 			preferencesSlide.setOnSlideHidden(() -> { if (preferencesSlide.isSaved()) hide(); });
 			preferencesSlide.showControls();
