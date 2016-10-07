6ae727d500e9792650399ee312c2c5a8ff1683e4
phrack <code.realm@gmail.com>
2016-10-07 19:53:03 -0400

    Remove unused imports

--- src/main/java/com/shootoff/gui/pane/TargetDistancePane.java
+++ src/main/java/com/shootoff/gui/pane/TargetDistancePane.java
@@ -40,7 +40,6 @@ import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 public class TargetDistancePane extends Pane {
 	private final static Logger logger = LoggerFactory.getLogger(TargetDistancePane.class);
