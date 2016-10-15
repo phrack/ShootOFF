a8fcc682ec62ed3b72f92ca6627fb5f064cfe958
phrack <code.realm@gmail.com>
2016-10-15 13:05:56 -0400

    Add additional backgrounds to close #630

--- src/main/java/com/shootoff/gui/pane/ArenaBackgroundsSlide.java
+++ src/main/java/com/shootoff/gui/pane/ArenaBackgroundsSlide.java
@@ -73,8 +73,10 @@ public class ArenaBackgroundsSlide extends Slide implements ItemSelectionListene
 			addDefaultBackground("Hickok45 Summer", "/arena/backgrounds/hickok45_summer.gif");
 			addDefaultBackground("Indoor Range", "/arena/backgrounds/indoor_range.gif");
 			addDefaultBackground("Kiang West Savanna", "/arena/backgrounds/kiang_west_savanna.gif");
+			addDefaultBackground("Oradour-sur-Glane", "/arena/backgrounds/oradour-sur-glane.gif");
 			addDefaultBackground("Outdoor Range", "/arena/backgrounds/outdoor_range.gif");
 			addDefaultBackground("Steel Range Bay", "/arena/backgrounds/steel_range_bay.gif");
+			addDefaultBackground("Subterranean Parking Lot", "/arena/backgrounds/subterranean_parking_lot.gif");
 		}).start();
 	}
 
