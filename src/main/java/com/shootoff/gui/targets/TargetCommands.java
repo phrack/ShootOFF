05ad579916c411b69cea885b721e5ab1d82d02f0
phrack <code.realm@gmail.com>
2017-03-05 09:52:42 -0500

    Add missing license header

--- src/main/java/com/shootoff/gui/targets/TargetCommands.java
+++ src/main/java/com/shootoff/gui/targets/TargetCommands.java
@@ -1,3 +1,21 @@
+/*
+ * ShootOFF - Software for Laser Dry Fire Training
+ * Copyright (C) 2016 phrack
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.shootoff.gui.targets;
 
 import java.io.BufferedInputStream;
@@ -17,21 +35,24 @@ import com.shootoff.targets.RegionType;
 import com.shootoff.targets.Target;
 import com.shootoff.targets.TargetRegion;
 
+/**
+ * This class contains the implementations for tag commands for targets.
+ */
 public class TargetCommands implements CommandProcessor {
 	private static final Logger logger = LoggerFactory.getLogger(TargetCommands.class);
-	
+
 	private final List<Target> targets;
 	private final Resetter resetter;
 	private final Hit hit;
 	private final Configuration config;
-	
+
 	public TargetCommands(List<Target> targets, Resetter resetter, Hit hit) {
 		this.targets = targets;
 		this.resetter = resetter;
 		this.hit = hit;
 		config = Configuration.getConfig();
 	}
-	
+
 	@Override
 	public void process(List<String> commands, String commandName, List<String> args) {
 		switch (commandName) {
@@ -52,8 +73,8 @@ public class TargetCommands implements CommandProcessor {
 			// if it's an image region that is down and if so, don't
 			// play the sound
 			if (args.size() == 2) {
-				final Optional<TargetRegion> namedRegion = TargetView.getTargetRegionByName(targets,
-						hit.getHitRegion(), args.get(1));
+				final Optional<TargetRegion> namedRegion = TargetView.getTargetRegionByName(targets, hit.getHitRegion(),
+						args.get(1));
 				if (namedRegion.isPresent() && namedRegion.get().getType() == RegionType.IMAGE) {
 					if (!((ImageRegion) namedRegion.get()).onFirstFrame()) break;
 				}
@@ -76,6 +97,6 @@ public class TargetCommands implements CommandProcessor {
 			}
 
 			break;
-		}	
+		}
 	}
 }
